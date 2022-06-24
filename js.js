c2548fbe842997b8ce3ddc95e8c688de9c76c230
crestcere@outlook.com <31207969+crestcere@users.noreply.github.com>
2022-06-24 09:25:19 +0300

    Functions, limit empty usage, some improvements

--- js.js
+++ js.js
@@ -1,5 +1,17 @@
 function operate() {
-    
+    switch (arguments[0]) {
+        case "+":
+            result += +value["input" + (val_index - 1)].join("") + +value["input" + val_index].join("");
+            break;
+        case "-":
+            result -= +value["input" + (val_index - 1)].join("") - +value["input" + val_index].join("");
+            // result += +value["input" + val_index].join("") - +value["input" + (val_index - 1)].join("");
+        default:
+            break;
+    }
+    // console.log(Object.keys(value).length);
+    // console.log("result: " + result);
+    console.table(value);
 }
 const functions = {
     add() {
@@ -15,31 +27,17 @@ const functions = {
         return arguments[0] / arguments[1];
     }
 }
-// add();
-// subtract();
-// multiply();
-// divide();
 
+// Display container create
 let display = tempElement("display", document.body, "div");
-
-// let display = document.createElement("div");
-// display.classList.add("display");
-// document.body.appendChild(display);
-
 function display_value() {
     display.textContent = arguments[0];
 }
-
 display_value("0");
 
 let container_1 = tempElement("container_1", document.body, "div")
-// let container_1 = document.createElement("div");
-// display.classList.add("container_1");
-// document.body.appendChild(container_1);
-// container_1.textContent = "aaa";
 
-
-
+// Number button and row div create
 for (let x = 0; x < 3; x++) {
     let b_row = document.createElement("div");
     b_row.classList.add("b_row");
@@ -53,14 +51,8 @@ for (let x = 0; x < 3; x++) {
         (x == 2) ? (k = y + 1) : (x == 1 ) ? (k = 4 + y) : (x == 0) ? (k = 7 + y) : (1);
         b_column.textContent = k;
         b_column.setAttribute("id", "b_column" + "#" + k);
-        /* (y < 3) ? (b_column.textContent = k) : (b_column.classList.toggle("b_operator"), b_column.classList.toggle("b_column"), 
-            ((x == 0) ? (b_column.textContent = "+") : (x == 1) ? (b_column.textContent = "-") : 
-            (x == 2) ? (b_column.textContent = "/") : (b_column.textContent = "*")));
-          */
-        // b_column.textContent = "+"
     }
 }
-
 {
     let b_row = document.createElement("div");
     b_row.setAttribute("class", "b_row");
@@ -68,6 +60,7 @@ for (let x = 0; x < 3; x++) {
     container_1.appendChild(b_row);
 }
 
+// Zero, clear and operate button create
 {
     for (let i = 0; i < 3; i++) {
         let functions = document.createElement("button");
@@ -80,11 +73,12 @@ for (let x = 0; x < 3; x++) {
     }
 }
 
+// Function buttons create and assign text values
 {
     for (let i = 0; i < 4; i++) {
         let functions = document.createElement("button");
         functions.setAttribute("class", "functions");
-        functions.setAttribute("id", "functions#" + i);
+        functions.setAttribute("id", "functions#" + (i + 1));
         let aa = ["+", "-", "/", "*"];
         functions.textContent = aa[i];
         let aaa = document.getElementsByClassName("b_row");
@@ -92,10 +86,7 @@ for (let x = 0; x < 3; x++) {
     }
 }
 
-
-
-
-
+// Tempelement
 function tempElement(classname, mounts, type, many) {
     let classnames = classname;
     (typeof many == "undefined") ? (many = 1) : (many);
@@ -108,96 +99,89 @@ function tempElement(classname, mounts, type, many) {
     return classname;
 }
 
+// Values and operators
 let value = {
     input1 : []
 };
 
+let operator = {};
+
 let val_index = 1;
+
+let operator_index = 1;
 
 let result = 0;
 
-
-for (let i = 0; i < 10; i++) {
-    let b_column;
-    if (i == 9) {
-        b_column = document.getElementsByClassName("zeros")[0];
-    } else {
-        b_column = document.getElementsByClassName("b_column")[i];
-    }
+// Numbers event listener
+for (let i = 1; i < 10; i++) {
+    let b_column = document.getElementById("b_column#" + i);
     b_column.addEventListener("click", () => {
-        // display_value(b_column.textContent);        
-        // value.input1 = [];
         let a = "input" + val_index;
+        // console.log("a: " + a);
         value[a].push(b_column.textContent);
         display_value(value[a].join(""));
     })
 }
 
+// Zero button event listener;
+document.getElementById("zeros#0").addEventListener("click", () => {
+    let a = "input" + val_index;
+    value[a].push("0");
+    display_value(value[a].join(""));
+})
+
+// Clear button event listener;
 let c_button = document.getElementById("zeros#1");
 c_button.addEventListener("click", () => {
     value = {
         input1 : []
     };
     val_index = 1;
+    result = 0;
     display_value("0");
 })
 
 function value_create() {
-    val_index++;
-    let a = "input" + val_index;
-    return value[a] = [];
+    if (arguments[0] == "val") {
+        // val_index++;
+        let a = "input" + val_index;
+        value[a] = [];
+    }
+    else if (arguments[0] == "op") {
+        let b = "input_op" + operator_index;
+        value[b] = [];
+        value[b].push(arguments[1]);
+        console.log("op index: " + operator_index);
+        console.log("val index: " + val_index);
+        operator_index++;
+        val_index++;
+    }
 }
 
-
+// Add operator event listener;
 let add = document.getElementsByClassName("functions")[0];
     add.addEventListener("click", () => {
-    value_create();
-    console.log("add clicked");
+    // value_create("op", add.textContent);
+    if (value["input" + val_index].length > 0) {
+        value_create("op", "+");
+        value_create("val");
+        operate("+");
+        display_value("0");
+        console.table(value);
+    }
 })
+
+// Subtract operator event listener;
 let subtract = document.getElementsByClassName("functions")[1];
+subtract.addEventListener("click", () => {
+    value_create("op", "-");
+    value_create("val");
+    operate("+");
+    display_value("0");
+    console.table(value);
+})
 
-
-// let a = document.createElement("button");
-// a.classList.add("b_row");
-// container_1.appendChild(a);
-
-// let b_row = document.getElementsByClassName("b_row")[0];
-
-// let zeros;
-// tempElement(b_row, container_1, "div");
-
-// let zeros = tempElement("zeros", container_1, "button", 3);
-// document.getElementById("zeros#0").textContent = 0;
-// document.getElementById("zeros#1").textContent = "C";
-// document.getElementById("zeros#2").textContent = "="
-
-// let functions = tempElement("functions", document.getElementById("b_row#0"), "button", 4);
-
-// for (let i = 0; i < )
-
-// let aasd = document.createElement("div");
-// zeros.appendChild(aasd);
-
-// console.log(zeros);
-
-// for (let i = 0; i < 4; i++) {
-//     tempElement("f_keys", "button", b_row[i], "f_keys");
-//     let a = document.getElementsByClassName("f_keys");
-//     console.log(a);
-//     b_row.appendChild(a);
-//     switch (i) {
-//         case 0:
-//             a.textContent = "+";
-//             break;
-//         default:
-//             a.textContent = "UwU"    
-//     }
-// }
-// tempElement("f_keys", "button", b_row[2], 1);
-
-
-
-// b_row[4].textContent = "aa";
-// b_row[4].appendChild(aa);
-
-// console.table(b_row);
+// Operate button event listener;
+document.getElementById("zeros#2").addEventListener("click", () => {
+    display_value(result);
+})
